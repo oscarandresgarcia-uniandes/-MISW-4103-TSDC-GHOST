96d217f7929e4a8dbf78f249a89a356210757aaa
Andres Garcia <oa.garcia2@uniandes.edu.co>
2023-11-12 21:13:32 -0500

    Asegurar que se haga scroll completo para evitar errores cuando hay listas largas y lazy loading

--- cypress-tests/cypress/e2e/page-object/PageListPage.js
+++ cypress-tests/cypress/e2e/page-object/PageListPage.js
@@ -6,21 +6,28 @@ class PageListPage {
     }
 
     pageExists(pageData, status = 'Draft') {
+      cy.window().scrollTo('bottom', { ensureScrollable: false });
+      cy.wait(500);
       cy.get('.gh-content-entry-title').contains(pageData.title)
       .parent()
       .find('.gh-content-entry-status').contains(status);
     }
 
     pageNotExist(pageData) {
+      cy.window().scrollTo('bottom', { ensureScrollable: false });
+      cy.wait(500);
       cy.get('.gh-content-entry-title').contains(pageData.title).should('not.exist');
     }
 
     navigateToPageByTitle(title) {
+      cy.window().scrollTo('bottom', { ensureScrollable: false });
+      cy.wait(1000);
       cy.get('.gh-posts-list-item .gh-content-entry-title').contains(title).click();
     }
 
     filterbyTag(tagName){
-      cy.wait(1000);
+      cy.window().scrollTo('bottom', { ensureScrollable: false });
+      cy.wait(500);
       cy.get('.gh-contentfilter-tag').click();
       cy.get('.ember-power-select-options li').contains(tagName).click();
     }
--- cypress-tests/cypress/e2e/page-object/TagListPage.js
+++ cypress-tests/cypress/e2e/page-object/TagListPage.js
@@ -10,16 +10,19 @@ class TagListPage {
     }
 
     navigateToEditTag(tagData) {
+        cy.window().scrollTo('bottom', { ensureScrollable: false });
         cy.get('.gh-tags-list-item .gh-tag-list-name').contains(tagData.name).click();
     }
 
     tagExists(tagData) {
+        cy.window().scrollTo('bottom', { ensureScrollable: false });
         cy.get('.gh-tags-list-item .gh-tag-list-name').contains(tagData.name);
         cy.get('.gh-tags-list-item .gh-tag-list-description').contains(tagData.description);
         cy.get('.gh-tags-list-item [data-test-tag-slug] span').contains(tagData.slug);
     }
 
     tagNotExist(tagData) {
+        cy.window().scrollTo('bottom', { ensureScrollable: false });
         cy.get('.gh-tags-list-item .gh-tag-list-name').should('not.contain', tagData.name);
         cy.get('.gh-tags-list-item .gh-tag-list-description').should('not.contain', tagData.description);
         cy.get('.gh-tags-list-item [data-test-tag-slug] span').should('not.contain', tagData.slug);
