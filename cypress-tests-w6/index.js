fd42bcf6d82936f494f3ab02e149044701b71b19
Andres Garcia <oa.garcia2@uniandes.edu.co>
2023-11-17 15:36:06 -0500

    Compare script

--- cypress-tests-w6/index.js
+++ cypress-tests-w6/index.js
@@ -1,5 +1,89 @@
 const fs = require('fs');
 const path = require('path');
+const compareImages = require('resemblejs/compareImages');
+const config = require('./config.json');
+const { options } = config;
+
+// Escribe el HTML con el reporte
+async function renderGhostReport(imagesA, imagesB, folderPathA, folderPathB) {
+
+  const datetime = new Date().toISOString().replace(/:/g,".");
+
+  if (!fs.existsSync(`./results/${datetime}`)){
+    fs.mkdirSync(`./results/${datetime}`, { recursive: true });
+    fs.mkdirSync(`./results/${datetime}/images/`, { recursive: true });
+  }
+
+  fs.copyFileSync('./styles.css', `./results/${datetime}/styles.css`);
+
+  const htmlOutput = fs.createWriteStream(`./results/${datetime}/reporte.html`);
+
+  htmlOutput.write(
+    `<html>
+    <head>
+      <link href="styles.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+      <table>
+        <tr><th>Ghost 4.48.9</th><th>Ghost 5.73.2</th></tr>
+        `
+    );
+
+  // Busca imagenes con el mismo nombre
+  for (const imagePathA of imagesA) {
+    const relativePathA = path.relative(folderPathA, imagePathA);
+    const correspondingImageB = path.join(folderPathB, relativePathA);
+    const regex = /\/cypress\/screenshots\/(.+\.spec\.cy\.js\/.+)\.png/;
+    const match = imagePathA.match(regex);
+    const imageRoute = match[1];
+    const compareImageName = generateImageCompareName();
+
+    if (imagesB.includes(correspondingImageB)) {
+
+
+      //Comparar con resemble
+      const data = await compareImages(
+        fs.readFileSync(imagePathA),
+        fs.readFileSync(correspondingImageB),
+        options
+    );
+
+    compareInfo = {
+      isSameDimensions: data.isSameDimensions,
+      dimensionDifference: data.dimensionDifference,
+      rawMisMatchPercentage: data.rawMisMatchPercentage,
+      misMatchPercentage: data.misMatchPercentage,
+      diffBounds: data.diffBounds,
+      analysisTime: data.analysisTime
+    }
+
+    fs.writeFileSync(`./results/${datetime}/images/${compareImageName}`, data.getBuffer());
+
+    htmlOutput.write(`
+      <tr>
+        <td colspan="2">
+          <strong>${imageRoute}</strong>
+          <p>Data: ${JSON.stringify(compareInfo)}</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <img width="75%" src="../../${imagePathA}">
+        </td>
+        <td>
+          <img width="75%" src="../../${correspondingImageB}">
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2"> <img width="75%" src="images/${compareImageName}"></td>
+      </tr>
+    `);
+    }
+  }
+
+  htmlOutput.write('</table></body></html>');
+  htmlOutput.end();
+}
 
 //Inicializa el reporte
 function createReport(folderPathA, folderPathB) {
@@ -26,47 +110,11 @@ function readFiles(dir) {
   }, []);
 }
 
-// Escribe el HTML con el reporte
-function renderGhostReport(imagesA, imagesB, folderPathA, folderPathB) {
-  const datetime = new Date().toISOString().replace(/:/g,".");
-  const htmlOutput = fs.createWriteStream(`reporte-${datetime}.html`);
-  htmlOutput.write(
-    `<html>
-    <head>
-      <link href="styles.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-      <table>
-        <tr><th>Ghost 4.48.9</th><th>Ghost 5.73.2</th></tr>
-        `
-    );
+function generateImageCompareName() {
+  const timestamp = new Date().getTime();
+  const randomString = Math.random().toString(36).substring(2, 8);
 
-  // Busca imagenes con el mismo nombre
-  imagesA.forEach(imagePathA => {
-    const relativePathA = path.relative(folderPathA, imagePathA);
-    const correspondingImageB = path.join(folderPathB, relativePathA);
-    const regex = /\/cypress\/screenshots\/(.+\.spec\.cy\.js\/.+)\.png/;
-    const match = imagePathA.match(regex);
-    const imageRoute = match[1];
-
-    if (imagesB.includes(correspondingImageB)) {
-      htmlOutput.write(`
-        <tr>
-          <td colspan="2" style="text"><strong>${imageRoute}</strong></td>
-        </tr>
-        <tr>
-          <td>
-            <img width="75%" src="${imagePathA}">
-          </td>
-          <td>
-            <img width="75%" src="${correspondingImageB}">
-          </td>
-        </tr>`);
-    }
-  });
-
-  htmlOutput.write('</table></body></html>');
-  htmlOutput.end();
+  return `${timestamp}-${randomString}.png`;
 }
 
 //Inicializa el reporte
