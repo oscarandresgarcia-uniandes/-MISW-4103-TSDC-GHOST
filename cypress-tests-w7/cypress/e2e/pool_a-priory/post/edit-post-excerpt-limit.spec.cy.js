f5273651fd4d5c400381e24930e7c547cbf194db
Paula Bohorquez <extern.Paula.Bohorquez@audi.com>
2023-11-26 01:17:20 -0500

    APRIORI - Editar post excediendo caracteres en excerpt

---
+++ cypress-tests-w7/cypress/e2e/pool_a-priory/post/edit-post-excerpt-limit.spec.cy.js
@@ -0,0 +1,37 @@
+import PostNewPage from '../../page-object/PostNewPage';
+import PostListPage from '../../page-object/PostListPage';
+
+describe('Edición de un Post excediendo limite en excerpt', function() {
+    
+    const postPage = new PostNewPage();
+    const postListPage = new PostListPage();
+
+    it('Test para editar un Post con un excerpt que exceda la cantidad de caracteres', function() {
+
+        const dataPool = this.dataPool;
+        const postTitle = dataPool['page-post'][0].title
+        const postTitleExceed = dataPool['page-post'][2].plaintext
+        const postContent = dataPool['page-post'][0].plaintext
+
+        //Se accede a página de creación de Posts
+        postPage.visit();
+        
+        //Se crea un nuevo post
+        postPage.createPost(postTitle,postContent)
+        postPage.submitPost('Publish')
+        cy.wait(3000)
+        
+        //Se verifica que el post haya sido creado 
+        postListPage.visit()
+
+        //Se edita el post para exceder cantidad de caracteres en el título 
+        postPage.visitEdit(postTitle);
+        postPage.editExcerpt(postTitleExceed)
+        postPage.submitPost('Publish leave')
+        
+
+        //Se presenta mensaje de error al exceder la cantidad límite de caracteres
+        postPage.validateLimitExcerpt()
+        
+    })
+  })
